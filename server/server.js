eaaac4a43a59de537c0ecebbd456bb4d3cdf0aea
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-06 14:32:45 -0700

    done

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+const authCtrl = require('./authController')
+
+const app = express()
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.delete('/auth/logout', authCtrl.logout)
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => console.log(`Listening on ${SERVER_PORT}`))
+})
